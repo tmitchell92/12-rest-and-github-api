3d58b6c90db69bc91c2041989ead37b1c0d0bcdf
timothy mitchell <timothy.p.mitchell92@gmail.com>
2017-05-23 13:23:18 -0700

    did repo.js todo

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -7,11 +7,21 @@ var app = app || {};
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+    // DONE: How would you like to fetch your repos? Don't forget to call the callback.
     //       Remember that the callback function we'll want to call relies on repos.all
     //       being an array with a bunch of repo objects in it, so you'll need to
     //       populate it with the response from Github before you call the callback.
-
+    $.ajax({
+      url: 'https://api.github.com/user/repos',
+      method: 'GET',
+      headers:{
+        Authorization: `token ${githubToken}`        }
+    })
+.then(
+  data => {
+    data.forEach(repos.all.push(data))}
+);
+    callback();
   };
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
